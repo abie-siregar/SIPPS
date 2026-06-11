ff93fe7340f92a28a3de1e605f6f73a8783c95e8
abie-siregar <habibullahsiregar9@gmail.com>
2026-06-12 02:43:35 +0700

    chore(generate): update generateController logic

--- backend/src/controllers/generateController.js
+++ backend/src/controllers/generateController.js
@@ -7,28 +7,31 @@ module.exports= {
     async ptk(req, res) {
     try {
         
-      const defaultPassword = "sipps1234"
+      const defaultPassword = "sipps@1234ptk"
       const hashedPassword = await bcrypt.hash(defaultPassword, 10);
 
       const query = `
-        INSERT INTO users (username, email, role_id, password, alamat, ptk_id_dapodik, is_active)
-        SELECT 
-            p.nuptk AS username,
-            p.email,
-            CASE 
-                WHEN p.jenis_ptk_id = 92 THEN 2
-                WHEN p.jenis_ptk_id = 93 THEN 3
-                WHEN r.rombel_id IS NOT NULL THEN 4
-                ELSE 99
-            END AS role_id,
-            $1 AS password,
-            p.alamat as alamat,
-            p.ptk_id_dapodik,
-            true
+        INSERT INTO users (id_role, id_ptk, username,  password,  is_active)
+        SELECT DISTINCT ON (p.id_ptk)
+        CASE 
+        WHEN p.id_jabatan = 10001 THEN 101
+        WHEN p.id_jabatan = 21904 THEN 102
+        WHEN r.id_ptk_wali IS NOT NULL THEN 103
+        WHEN CAST (p.id_jabatan AS TEXT) LIKE '2%' THEN 2
+        ELSE 3
+        END AS id_role,
+        p.id_ptk,
+        TRIM(p.email) AS username,
+        $1 AS password,
+        true AS is_active
         FROM ptk p
-        LEFT JOIN rombel r ON p.ptk_id_dapodik = r.ptk_id_dapodik
-        WHERE p.email IS NOT NULL
-        ON CONFLICT (ptk_id_dapodik) DO NOTHING
+        LEFT JOIN rombel r ON p.id_ptk = r.id_ptk_wali
+        WHERE p.email IS NOT NULL AND TRIM(p.email) != ''
+        ORDER BY p.id_ptk, r.id_rombel ASC
+        ON CONFLICT (username) DO UPDATE SET
+        id_role = EXCLUDED.id_role,
+        id_ptk = EXCLUDED.id_ptk,
+        is_active = EXCLUDED.is_active
         RETURNING *;
       `;
 
@@ -36,42 +39,34 @@ module.exports= {
 
       res.json({ message: "Users PTK berhasil digenerate", inserted: rows.length, data: rows });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Gagal generate user PTK" });
+      console.error("Gagal generate user PTK:", error.message);
+      res.status(500).json({ error: "Gagal generate user PTK: " + error.message });
     }
     },
 
     // Generate user dari Siswa
     async siswa(req, res) {
     try {
-      const defaultPassword = "sipps1234siswa"
+      const defaultPassword = "sipps@1234siswa"
       const hashedPassword = await bcrypt.hash(defaultPassword, 10);
 
       const query = `
         INSERT INTO 
           users (
-            username,
-            email,
             id_role,
+            id_siswa,
+            username,
             password,
-            alamat,
-            no_telepon,
-            no_hp,
-            siswa_id_dapodik,
             is_active
           )
       SELECT 
+        6 AS id_role,
+        s.id_siswa,
         s.nisn AS username,
-        COALESCE(s.email, s.nisn || '@sipps.com') AS email, -- jika email null, gunakan nisn@sipps.com
-        6 AS id_role, -- siswa selalu role 6
         $1 AS password,
-        s.alamat AS alamat,
-        s.nomor_telepon_rumah AS no_telepon,
-        s.nomor_telepon_seluler AS no_hp,   
-        s.siswa_id_dapodik,
-        true
+        true as is_active
       FROM siswa s
-      ON CONFLICT (siswa_id_dapodik) DO NOTHING
+      ON CONFLICT (username) DO NOTHING
       RETURNING *;
     `;
 
@@ -79,8 +74,8 @@ module.exports= {
 
       res.json({ message: "Users Siswa berhasil digenerate", inserted: rows.length, data: rows });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Gagal generate user Siswa" });
+      console.error("Gagal generate user Siswa:", error.message);
+      res.status(500).json({ error: "Gagal generate user Siswa: " + error.message });
     }
   },
 };
