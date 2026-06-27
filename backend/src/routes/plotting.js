1d7213116e2fd0e2502b55d08d874173ac8af367
abie-siregar <habibullahsiregar9@gmail.com>
2026-06-27 23:16:52 +0700

    Function: add Function getById on plottingController

--- backend/src/routes/plotting.js
+++ backend/src/routes/plotting.js
@@ -5,6 +5,7 @@ const isRoles = require("../middlewares/isRoles");
 
 router.post("/", isRoles(["Admin"]), plottingController.add);
 router.get("/", plottingController.getAll);
+router.post("/:id", isRoles(["Admin"]), plottingController.getById);
 router.put("/:id", isRoles(["Admin"]), plottingController.update);
 router.delete("/:id", isRoles(["Admin"]), plottingController.delete);
 
