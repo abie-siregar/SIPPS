1879ce7c567aedb456b9eec702f57b27cf722fb0
abie-siregar <habibullahsiregar9@gmail.com>
2026-07-11 00:39:32 +0700

    add reportController

--- backend/src/routes/report.js
+++ backend/src/routes/report.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const printController = require("../controllers/printController");
+const printController = require("../controllers/reportController");
 
 router.get("/", printController.PrintReport);
 
